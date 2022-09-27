26ff40d180be2e078f5f6596410c8577eae2d616
Aryan Barsaiyan <aryanbarsaiyan@gmail.com>
2022-09-27 17:04:09 +0530

    Progree BAR

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// SMOOTH SCROLL
+
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 var interval;
 for(var i=0; i<navMenuAnchorTags.length; i++) {
@@ -22,3 +24,45 @@ function scrollVertically(targetSection){
     prev=targetSectionCoordinates.top;
     window.scrollBy(0, 40);
 }
+
+
+// PROGRESS BAR
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById("skills-container");
+window.addEventListener('scroll', checkScroll);
+var animationDone = false;
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+function fillBar(bar) {
+    var currentWidth = 0;
+    var interval = setInterval(function () {
+        if (currentWidth >= bar.getAttribute("data-bar-width")) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+}
+function checkScroll() {
+    for(let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if((bar.getAttribute("data-visited") == "false") && (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        }
+        else if(barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+    }
+}
+
+
+
